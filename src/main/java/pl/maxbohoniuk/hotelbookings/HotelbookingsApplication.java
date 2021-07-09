ea02f2a3f08e8e5feb30bc54453c9073186c61d7
Max Bohoniuk <bohonyk1234@gmail.com>
2021-07-09 11:56:20 +0200

    Initial commit

---
+++ src/main/java/pl/maxbohoniuk/hotelbookings/HotelbookingsApplication.java
@@ -0,0 +1,13 @@
+package pl.maxbohoniuk.hotelbookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelbookingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelbookingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/maxbohoniuk/hotelbookings/HotelbookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maxbohoniuk.hotelbookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelbookingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
